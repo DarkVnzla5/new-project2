fbdabde037c33b4100dab8fef4f9908ad5f84652
DarkVnzla5 <felipeluna0117@outlook.es>
2026-03-31 20:27:10 -0400

    feat: initial commit

--- src/routes/_auth/login.tsx
+++ src/routes/_auth/login.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router"
-import { LoginPage } from "@/features/auth/login/page"
+import { LoginPage } from "@/features/auth/_login/page"
 
 export const Route = createFileRoute("/_auth/login")({
   component: LoginPage,
